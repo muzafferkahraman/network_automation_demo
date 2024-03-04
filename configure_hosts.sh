3975c50919d84260013c8d446f7b1f3d80a18a27
Muzaffer Kahraman <muzaffer.kahraman@outlook.com>
2024-03-04 12:45:09 +0300

    Delete configure_hosts.sh

--- configure_hosts.sh
+++
@@ -1,9 +0,0 @@
-docker exec -ti host10 ip li set dev eth1 mtu 1500
-docker exec -ti host10 ip li set dev eth1 address 00:00:00:01:01:00
-docker exec -ti host10 ip a add 192.168.100.15/24 dev eth1
-docker exec -ti host10 ip route add 192.168.200.0/24 via 192.168.100.5
-
-docker exec -ti host20 ip li set dev eth1 mtu 1500
-docker exec -ti host20 ip li set dev eth1 address 00:00:00:01:01:00
-docker exec -ti host20 ip a add 192.168.200.15/24 dev eth1
-docker exec -ti host20 ip route add 192.168.100.0/24 via 192.168.200.5
